b0f76ac1fe3078a345c01abe6e11ad8f2c618a28
MafaldaLandeiro <1110590@isep.ipp.pt>
2016-03-13 22:38:39 +0000

    Project creation

---
+++ AcessDataJPA/src/main/java/org/AcessDataJPA/App.java
@@ -0,0 +1,13 @@
+package org.AcessDataJPA;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
